f2ff1a8a43db36a46f032e21c4116becea65dda7
mudongcc <117074024+mudongcc@users.noreply.github.com>
2023-03-03 09:28:17 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -67,11 +67,7 @@
       </div>
       <hr />
       <div class="item" @click="toGitHub">
-        <svg-icon icon-class="github" />
         {{ $t('nav.github') }}
-      </div>
-      <div class="item">
-        <a href="https://dmusic.mudong.cc/">全网音乐免费下载平台</a>
       </div>
     </ContextMenu>
   </div>
